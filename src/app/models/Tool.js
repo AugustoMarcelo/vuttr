0f4ee6796f8f8df6cf345ac6a1dd2c5b1afca4bb
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-10 22:51:11 -0300

    Created Tool Model

---
+++ src/app/models/Tool.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require('mongoose');
+
+const ToolSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+      required: true,
+    },
+    tags: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    timestamps: true, // Cria automaticamente createAt e updateAt
+  }
+);
+
+module.exports = model('Tool', ToolSchema);
