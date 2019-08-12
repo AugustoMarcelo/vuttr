e326d3d64cb87f79ab405535b9566dcb1c439a5a
AugustoMarcelo <mrclgst10@gmail.com>
2019-08-12 20:32:41 -0300

    Criação do métos index e destroy

--- src/app/controllers/ToolController.js
+++ src/app/controllers/ToolController.js
@@ -4,7 +4,32 @@ class ToolController {
   async store(request, response) {
     const tool = await Tool.create(request.body);
 
-    return response.json(tool);
+    return response.status(201).json(tool);
+  }
+
+  async index(request, response) {
+    let tools;
+    // Checa se há query params
+    if (request.query.tag) {
+      const { tag } = request.query;
+
+      tools = await Tool.find({
+        tags: { $in: tag },
+      });
+
+    } else {
+      tools = await Tool.find();
+    }
+
+    return response.status(tools.length > 0 ? 200 : 204 ).json(tools);
+  }
+
+  async destroy(request, response) {
+    const { id } = request.params;
+
+    await Tool.deleteOne({ _id: id });
+
+    response.status(204).send();
   }
 }
 
